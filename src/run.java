36290e13cd1fdf4beb2260156adbff72a845ba9e
miatroiano <m11ssing1n4ction@gmail.com>
2022-02-27 16:31:39 -0500

    added fish

--- src/run.java
+++ src/run.java
@@ -11,7 +11,7 @@ public class run {
 		Boolean loop= true;
 		
 		while (loop == true) {
-			System.out.println("Would you like to look at a dog or cat?");
+			System.out.println("Would you like to look at a Dog, Fish, or Cat?");
 			String animals = scanner.nextLine();
 			if (animals.equalsIgnoreCase("dog")) {
 				System.out.println("   __\n        /  \\\n       / ..|\\\n      (_\\  |_)\n      /  \\@\' \n     /     \\\n_   /  `   |\n\\\\/  \\  | _\\\n \\   /_ || \\\\_\n  \\____)|_) \\_)"
@@ -22,6 +22,12 @@ public class run {
 			else if(animals.equalsIgnoreCase("cat")) {
 				System.out.println(
 						"  |\\---/|\n   | ,_, |\n    \\_`_/-..----.\n ___/ `   \' ,\"\"+ \\  sk\n(__...\'   __\\    |`.___.\';\n  (_,...\'(_,.`__)/\'.....+"
+						);
+				loop = false;
+			}
+			else if(animals.equalsIgnoreCase("fish")) {
+				System.out.println(
+						"|\\   \\\\\\\\__     o\n| \\_/    o \\    o \n> _   (( <_  oo  \n| / \\__+___/      \n|/     |/"
 						);
 				loop = false;
 			}
