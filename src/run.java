73adcf2f79c7c865d47844685ad34b8d229e79e1
miatroiano <m11ssing1n4ction@gmail.com>
2022-02-27 16:20:18 -0500

    run until valid input

--- src/run.java
+++ src/run.java
@@ -1,21 +1,31 @@
 
 import java.util.Scanner;
 
+
+
 public class run {
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Would you like to look at a dog or cat?");
-		String animals = scanner.nextLine();
-		if (animals.equalsIgnoreCase("dog")) {
-			System.out.println("DOG ASCII ART HERE");
-			
+		
+		Boolean loop= true;
+		
+		while (loop == true) {
+			System.out.println("Would you like to look at a dog or cat?");
+			String animals = scanner.nextLine();
+			if (animals.equalsIgnoreCase("dog")) {
+				System.out.println("DOG ASCII ART HERE");
+				loop = false;
+				
+			}
+			else if(animals.equalsIgnoreCase("cat")) {
+				System.out.println("“CAT ASCII ART HERE");
+				loop = false;
+			}
+			else {
+				System.out.println("invalid option!");
+			}
 		}
-		else if(animals.equalsIgnoreCase("cat")) {
-			System.out.println("“CAT ASCII ART HERE");
-		}
-		else {
-			System.out.println("invalid option!");
-		}
+		
 	}
 }
