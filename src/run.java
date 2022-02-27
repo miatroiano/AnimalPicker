1b558462dd4acc7188d0f7a46e7ce4c64a4dc479
miatroiano <m11ssing1n4ction@gmail.com>
2022-02-27 16:16:48 -0500

    invalid option error

--- src/run.java
+++ src/run.java
@@ -1,4 +1,21 @@
 
-public class run {
+import java.util.Scanner;
 
+public class run {
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Would you like to look at a dog or cat?");
+		String animals = scanner.nextLine();
+		if (animals.equalsIgnoreCase("dog")) {
+			System.out.println("DOG ASCII ART HERE");
+			
+		}
+		else if(animals.equalsIgnoreCase("cat")) {
+			System.out.println("“CAT ASCII ART HERE");
+		}
+		else {
+			System.out.println("invalid option!");
+		}
+	}
 }
